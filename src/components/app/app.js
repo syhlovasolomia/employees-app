a5a1b507618bb4b14694c6f0e2d61e276581140f
syhlovasolomia <syhlova.solomia@gmail.com>
2024-10-08 21:00:38 +0300

    added additional library for working with classes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,17 +7,24 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+  const data = [
+    { name: 'John C.', salary: 1000, increase: false},
+    { name: 'Alice M.', salary: 3000, increase: true},
+    { name: 'Lisa O.', salary: 5000,  increase: false}
+  ];
+
   return (
     <div className="app">
-        <AppInfo />
+      <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+
+      <EmployeesList data={data}/>
+      <EmployeesAddForm />
     </div>
   );
 }
